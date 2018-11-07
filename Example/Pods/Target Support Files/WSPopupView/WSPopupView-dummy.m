f948fa77ede8fd83798ba7263fb0fed006242f57
wangsai <wangsai@mia.com>
2018-11-07 22:55:13 +0800

    “xxx”

---
+++ Example/Pods/Target Support Files/WSPopupView/WSPopupView-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_WSPopupView : NSObject
+@end
+@implementation PodsDummy_WSPopupView
+@end
--- Example/WSPopupView/WSViewController.m
+++ Example/WSPopupView/WSViewController.m
@@ -7,7 +7,7 @@
 //
 
 #import "WSViewController.h"
-
+#import <WSPopupView/WSPopupView.h>
 @interface WSViewController ()
 
 @end
@@ -17,7 +17,25 @@
 - (void)viewDidLoad
 {
     [super viewDidLoad];
-	// Do any additional setup after loading the view, typically from a nib.
+
+    UIView *view = [[UIView alloc] initWithFrame:CGRectMake(0, 0, 200, 400)];
+    view.backgroundColor = [UIColor clearColor];
+    
+    UIView *view1 = [[UIView alloc] initWithFrame:CGRectMake(0, 0, 200, 200)];
+    view1.backgroundColor = [UIColor redColor];
+    [view addSubview:view1];
+    
+    UIButton *button = [UIButton buttonWithType:UIButtonTypeCustom];
+    button.frame = CGRectMake(70, 300, 60, 60);
+    [button setBackgroundColor:[UIColor redColor]];
+    [button addTarget:self action:@selector(aaa) forControlEvents:UIControlEventTouchUpInside];
+    [view addSubview:button];
+    
+    WSPopupView *popView = [WSPopupView popupViewWithContentView:view showType:WSPopupShowTypeSlideFromTop hiddenType:WSPopupHiddenTypeSlideToTop];
+    popView.backgroundAlpfa = 0.4;
+    popView.duration = 10;
+    popView.hiddenOnBackgroundTouch = YES;
+    [popView showInView:self.view animated:YES];
 }
 
 - (void)didReceiveMemoryWarning
